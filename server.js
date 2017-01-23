169f07daff2168683015d35808c72d597e0d39ec
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-01-23 22:15:46 +0200

    anager reply

--- server.js
+++ server.js
@@ -151,6 +151,7 @@ slapp.action('manager_confirm_reject', 'confirm', (msg, value) => {
 
 })
 slapp.action('manager_confirm_reject', 'reject', (msg, value) => {
+  msg.say("you hava rejected the vacation request")
   pg.connect(process.env.Db_URL, function (err, client) {
     if (err) throw err;
     console.log('Connected to postgres! Getting schemas...');
