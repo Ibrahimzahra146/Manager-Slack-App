1c0e0a5b0a0fa0f59579517aab4344f7fcc28163
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-04-04 10:19:45 +0300

    getnewsessionwithcookies

--- server.js
+++ server.js
@@ -229,7 +229,15 @@ function sendRequestToApiAi(emailValue, msg) {
 
           }
           else if (response.result.parameters.email) {
-            employeeEmail = response.result.parameters.email
+            if ((response.result.parameters.email).indexOf('mailto') > -1) {
+              employeeEmail = response.result.parameters.email
+              employeeEmail = employeeEmail.toString().split('|')
+              employeeEmail = employeeEmail[1];
+              employeeEmail = employeeEmail.replace(/>/g, "");
+              console.log("Email after split mail to ")
+            }
+            else employeeEmail = response.result.parameters.email
+
 
 
 
