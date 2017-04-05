39d806fa0a76c3fbe0f8b19ab9a492936db30d51
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-04-05 17:04:08 +0300

    getnewsessionwithcookies

--- managerToffyHelper.js
+++ managerToffyHelper.js
@@ -515,6 +515,7 @@ module.exports.sendVacationToManager = function sendVacationToManager(startDate,
             var x = managerToffyHelper.getEmailById('employee/email/' + managerApproval[i].manager, email, function (emailFromId) {
                 console.log("email != emailFromId)" + email + "!=" + emailFromId)
                 emailFromId = emailFromId.replace(/\"/, "")
+                console.log("email != emailFromId)" + email + "!=" + emailFromId)
 
                 if (email != emailFromId) {
                     console.log("Arrive  after get emailFromId:: " + i)
