75b95bb72636a099ec2af0e3c43a02128dbec72f
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-04-13 11:24:26 +0300

    caneled requst effect

--- server.js
+++ server.js
@@ -92,7 +92,8 @@ function sendFeedBackMessage(responseBody) {
   });
 }
 
-function sendVacationPutRequest(vacationId, approvalId, managerEmail, status) {
+function sendVacationPutRequest(vacationId, approvalId, managerEmail, status, callback) {
+  var isDeleted = false;
 
   managerToffyHelper.getNewSessionwithCookie(managerEmail, function (remember_me_cookie, session_id) {
     managerToffyHelper.general_remember_me = remember_me_cookie;
@@ -117,8 +118,11 @@ function sendVacationPutRequest(vacationId, approvalId, managerEmail, status) {
       body: approvalBody
       //Set the body as a stringcc
     }, function (error, response, body) {
+      if (response.statusCode == 404) {
+        isDeleted = true
+      }
       console.log("arrive at get new POST requst " + response.statusCode)
-
+      callback(isDeleted)
     });
   })
 
@@ -624,111 +628,120 @@ function managerApproval1(msg, value, approvalType, fromManager) {
   console.log("fromManager" + fromManager)
   console.log("userEmail  ::" + userEmail)
   console.log("managerEmail::" + managerEmail)
-  sendVacationPutRequest(vacationId, approvalId, managerEmail, approvalType)
-  if (fromManager != 1) {
-    request({
-      url: 'http://' + IP + '/api/v1/toffy/get-record', //URL to hitDs
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Cookie': 'JSESSIONID=24D8D542209A0B2FF91AB2A333C8FA70'
-      },
-      body: userEmail
-      //Set the body as a stringcc
-    }, function (error, response, body) {
-      var responseBody = JSON.parse(body);
-      managerToffyHelper.getNewSessionwithCookie(managerEmail, function (remember_me_cookie, session_id) {
+  sendVacationPutRequest(vacationId, approvalId, managerEmail, approvalType, function (isDeleted) {
+    if (isDeleted == false) {
 
 
-        var uri = 'http://' + IP + '/api/v1/vacation/' + vacationId
+
+
+      if (fromManager != 1) {
         request({
-          url: uri, //URL to hitDs
-          method: 'GET',
+          url: 'http://' + IP + '/api/v1/toffy/get-record', //URL to hitDs
+          method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            'Cookie': remember_me_cookie + ";" + session_id
-
-          }
+            'Cookie': 'JSESSIONID=24D8D542209A0B2FF91AB2A333C8FA70'
+          },
+          body: userEmail
           //Set the body as a stringcc
         }, function (error, response, body) {
-          console.log("email:" + body)
-          console.log("Vacation state is :::" + JSON.stringify(body))
-
-          console.log("(JSON.parse(body)).vacationState)" + (JSON.parse(body)).vacationState)
+          var responseBody = JSON.parse(body);
+          managerToffyHelper.getNewSessionwithCookie(managerEmail, function (remember_me_cookie, session_id) {
 
 
+            var uri = 'http://' + IP + '/api/v1/vacation/' + vacationId
+            request({
+              url: uri, //URL to hitDs
+              method: 'GET',
+              headers: {
+                'Content-Type': 'application/json',
+                'Cookie': remember_me_cookie + ";" + session_id
 
-
-          if (approvalType == "ApprovedWithoutDeduction") {
-            if ((JSON.parse(body)).vacationState == "Rejected") {
-              userFeedbackmessage = "The approver " + managerEmail + " has accepted your time off request without detuction ( " + fromDate + " - " + toDate + " ). Your time off is rejected. "
-            } else if ((JSON.parse(body)).vacationState == "Approved") {
-              userFeedbackmessage = "The approver " + managerEmail + " has accepted your time off request without detuction ( " + fromDate + " - " + toDate + " ). Your time off is approved. "
-            } else if ((JSON.parse(body)).vacationState == "ApprovedWithoutDeduction") {
-              userFeedbackmessage = "The approver " + managerEmail + " has accepted your time off request without detuction ( " + fromDate + " - " + toDate + " ). Your time off is approved but without detuction. "
-
-
-            } else userFeedbackmessage = "The approver " + managerEmail + " has accepted your time off request without detuction ( " + fromDate + " - " + toDate + " ).Please wait other approvers to take an action"
-
-            managerFeedbackmessage = "You have accepted the time off request but without detuction"
-            msg.say(managerFeedbackmessage)
-          } else if (approvalType == "Approved") {
-
-            if ((JSON.parse(body)).vacationState == "Rejected") {
-              userFeedbackmessage = "The approver " + managerEmail + " has Approved your time off ( " + fromDate + " - " + toDate + " ). Your time off is rejected. "
-            } else if ((JSON.parse(body)).vacationState == "Approved") {
-              userFeedbackmessage = "The approver " + managerEmail + " has Approved your time off request ( " + fromDate + " - " + toDate + " ). Your time off is approved. "
-            } else if ((JSON.parse(body)).vacationState == "ApprovedWithoutDeduction") {
-              userFeedbackmessage = "The approver " + managerEmail + " has Approved your time off request ( " + fromDate + " - " + toDate + " ). Your time off is approved but without detuction. "
-
-
-            } else userFeedbackmessage = "The approver " + managerEmail + " has Approved your time off request ( " + fromDate + " - " + toDate + " ).Please wait other approvers to take an action"
-
-            managerFeedbackmessage = "You have accepted the time off."
-            msg.say(managerFeedbackmessage)
-
-
-          } else if (approvalType == "Rejected") {
-            if ((JSON.parse(body)).vacationState == "Rejected") {
-              userFeedbackmessage = "The approver " + managerEmail + " has rejected your time off ( " + fromDate + " - " + toDate + " ). Your time off is rejected. "
-            } else if ((JSON.parse(body)).vacationState == "Approved") {
-              userFeedbackmessage = "The approver " + managerEmail + " has rejected your time off request ( " + fromDate + " - " + toDate + " ). Your time off is approved. "
-            } else if ((JSON.parse(body)).vacationState == "ApprovedWithoutDeduction") {
-              userFeedbackmessage = "The approver " + managerEmail + " has rejected your time off request ( " + fromDate + " - " + toDate + " ). Your time off is approved but withoit detuction. "
-
-
-            } else userFeedbackmessage = "The approver " + managerEmail + " has rejected your time off request ( " + fromDate + " - " + toDate + " ).Please wait other approvers to take an action"
-
-            managerFeedbackmessage = "You have regected the time off."
-            msg.say(managerFeedbackmessage)
-
-          }
-          var message = {
-            'type': 'message',
-            'channel': responseBody.userChannelId,
-            user: responseBody.slackUserId,
-            text: 'what is my name',
-            ts: '1482920918.000057',
-            team: responseBody.teamId,
-            event: 'direct_message'
-          };
-          bot.startConversation(message, function (err, convo) {
-
-
-            if (!err) {
-              var text12 = {
-                "text": userFeedbackmessage,
               }
-              var stringfy = JSON.stringify(text12);
-              var obj1 = JSON.parse(stringfy);
-              bot.reply(message, obj1);
+              //Set the body as a stringcc
+            }, function (error, response, body) {
+              console.log("email:" + body)
+              console.log("Vacation state is :::" + JSON.stringify(body))
 
-            }
-          });
-        });
-      })
-    })
-  }
+              console.log("(JSON.parse(body)).vacationState)" + (JSON.parse(body)).vacationState)
+
+
+
+
+              if (approvalType == "ApprovedWithoutDeduction") {
+                if ((JSON.parse(body)).vacationState == "Rejected") {
+                  userFeedbackmessage = "The approver " + managerEmail + " has accepted your time off request without detuction ( " + fromDate + " - " + toDate + " ). Your time off is rejected. "
+                } else if ((JSON.parse(body)).vacationState == "Approved") {
+                  userFeedbackmessage = "The approver " + managerEmail + " has accepted your time off request without detuction ( " + fromDate + " - " + toDate + " ). Your time off is approved. "
+                } else if ((JSON.parse(body)).vacationState == "ApprovedWithoutDeduction") {
+                  userFeedbackmessage = "The approver " + managerEmail + " has accepted your time off request without detuction ( " + fromDate + " - " + toDate + " ). Your time off is approved but without detuction. "
+
+
+                } else userFeedbackmessage = "The approver " + managerEmail + " has accepted your time off request without detuction ( " + fromDate + " - " + toDate + " ).Please wait other approvers to take an action"
+
+                managerFeedbackmessage = "You have accepted the time off request but without detuction"
+                msg.say(managerFeedbackmessage)
+              } else if (approvalType == "Approved") {
+
+                if ((JSON.parse(body)).vacationState == "Rejected") {
+                  userFeedbackmessage = "The approver " + managerEmail + " has Approved your time off ( " + fromDate + " - " + toDate + " ). Your time off is rejected. "
+                } else if ((JSON.parse(body)).vacationState == "Approved") {
+                  userFeedbackmessage = "The approver " + managerEmail + " has Approved your time off request ( " + fromDate + " - " + toDate + " ). Your time off is approved. "
+                } else if ((JSON.parse(body)).vacationState == "ApprovedWithoutDeduction") {
+                  userFeedbackmessage = "The approver " + managerEmail + " has Approved your time off request ( " + fromDate + " - " + toDate + " ). Your time off is approved but without detuction. "
+
+
+                } else userFeedbackmessage = "The approver " + managerEmail + " has Approved your time off request ( " + fromDate + " - " + toDate + " ).Please wait other approvers to take an action"
+
+                managerFeedbackmessage = "You have accepted the time off."
+                msg.say(managerFeedbackmessage)
+
+
+              } else if (approvalType == "Rejected") {
+                if ((JSON.parse(body)).vacationState == "Rejected") {
+                  userFeedbackmessage = "The approver " + managerEmail + " has rejected your time off ( " + fromDate + " - " + toDate + " ). Your time off is rejected. "
+                } else if ((JSON.parse(body)).vacationState == "Approved") {
+                  userFeedbackmessage = "The approver " + managerEmail + " has rejected your time off request ( " + fromDate + " - " + toDate + " ). Your time off is approved. "
+                } else if ((JSON.parse(body)).vacationState == "ApprovedWithoutDeduction") {
+                  userFeedbackmessage = "The approver " + managerEmail + " has rejected your time off request ( " + fromDate + " - " + toDate + " ). Your time off is approved but withoit detuction. "
+
+
+                } else userFeedbackmessage = "The approver " + managerEmail + " has rejected your time off request ( " + fromDate + " - " + toDate + " ).Please wait other approvers to take an action"
+
+                managerFeedbackmessage = "You have regected the time off."
+                msg.say(managerFeedbackmessage)
+
+              }
+              var message = {
+                'type': 'message',
+                'channel': responseBody.userChannelId,
+                user: responseBody.slackUserId,
+                text: 'what is my name',
+                ts: '1482920918.000057',
+                team: responseBody.teamId,
+                event: 'direct_message'
+              };
+              bot.startConversation(message, function (err, convo) {
+
+
+                if (!err) {
+                  var text12 = {
+                    "text": userFeedbackmessage,
+                  }
+                  var stringfy = JSON.stringify(text12);
+                  var obj1 = JSON.parse(stringfy);
+                  bot.reply(message, obj1);
+
+                }
+
+              });
+            });
+          })
+        })
+      }
+    }
+    else msg.say("Sorry,You can't take an action, since this time off request is already canceled.")
+  })
 }
 
 
