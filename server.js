c3b5a4010af4992917bf6bafac66a8f0299e5402
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-05-29 15:36:02 +0300

    manager approvals section

--- server.js
+++ server.js
@@ -899,11 +899,11 @@ function managerApproval1(msg, value, approvalType, fromManager, comment) {
             messageGenerator.generateManagerApprovelsSection(JSON.parse(body).managerApproval, managerEmail, function (managerApprovalsSection) {
 
               replaceMessage.replaceMessage(msg, userEmail, managerEmail, fromDate, toDate, type, approvalType, vacationId, approvalId, ImageUrl, typeText, workingDays, managerApprovalsSection, JSON.parse(body).vacationState)
-            })
-            messageSender.sendMessagetoEmpOnAction(msg, managerEmail, fromDate, toDate, userEmail, type, bot, approvalType, body, typeText, responseBody, comment);
 
-          });
+              messageSender.sendMessagetoEmpOnAction(msg, managerEmail, fromDate, toDate, userEmail, type, bot, approvalType, body, typeText, responseBody, comment);
 
+            });
+          })
         })
       }
     }
