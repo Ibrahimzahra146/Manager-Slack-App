c630ab390bff9025cf67832f990c47a9e07ec65e
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-05-30 13:54:46 +0300

    manager approvals section

--- messagesHelper/messageGenerator.js
+++ messagesHelper/messageGenerator.js
@@ -7,44 +7,49 @@ var IP = process.env.SLACK_IP
  * Genereta the approvers section when send the time off to them ,so any approvel can check the other approvels action
  */
 module.exports.generateManagerApprovelsSection = function generateManagerApprovelsSection(managerApproval, managerEmail, callback) {
+    if (!managerApproval[1]) {
+        callback("")
+    } else {
 
-    var i = 0
-    var size = Object.keys(managerApproval).length
-    var messageBody = ""
-    //Sorting Managers Approver based on rank
-    managerApproval.sort(function (a, b) {
-        return a.rank - b.rank;
-    });
-    console.log("Sorted " + size)
-    while (i < size) {
-        var flag = "true }"
-        if ((i + 1) == size) {
-            flag == "false }"
+
+        var i = 0
+        var size = Object.keys(managerApproval).length
+        var messageBody = ""
+        //Sorting Managers Approver based on rank
+        managerApproval.sort(function (a, b) {
+            return a.rank - b.rank;
+        });
+        console.log("Sorted " + size)
+        while (i < size) {
+            var flag = "true }"
+            if ((i + 1) == size) {
+                flag == "false }"
+            }
+            var arr = managerApproval[i].managerEmail.toString().split("@")
+            if (managerApproval[i].managerEmail != managerEmail) {
+                console.log("Arrivvve")
+                getEmoji(managerApproval[i].state, "", "", "", function (emoji) {
+
+
+                    messageBody = messageBody + "{" + "\"title\":" + "\"" + "Approver ( " + arr[0] + " )\"" + ",\"value\":" + "\"" + managerApproval[i].state + "" + emoji + "\"" + ",\"short\":" + flag
+                    messageBody = messageBody + ","
+                })
+            }
+
+            i++
         }
-        var arr = managerApproval[i].managerEmail.toString().split("@")
-        if (managerApproval[i].managerEmail != managerEmail) {
-            console.log("Arrivvve")
-            getEmoji(managerApproval[i].state, "", "", "", function (emoji) {
-
-
-                messageBody = messageBody + "{" + "\"title\":" + "\"" + "Approver ( " + arr[0] + " )\"" + ",\"value\":" + "\"" + managerApproval[i].state + "" + emoji + "\"" + ",\"short\":" + flag
-                messageBody = messageBody + ","
-            })
+        if (messageBody != "") {
+            messageBody = messageBody.replace(/}\"/g, "}")
+            messageBody = messageBody.replace(/\"\{/g, "{")
+            messageBody = messageBody.replace(/\\/g, "")
+            messageBody = messageBody.replace(/\",\"\"/g, "")
+            messageBody = messageBody.replace(/,,/, ",")
+            messageBody = messageBody.replace(/\"\{/g, "{")
+            console.log("messageBody:::" + messageBody)
         }
 
-        i++
+        callback(messageBody)
     }
-    if (messageBody != "") {
-        messageBody = messageBody.replace(/}\"/g, "}")
-        messageBody = messageBody.replace(/\"\{/g, "{")
-        messageBody = messageBody.replace(/\\/g, "")
-        messageBody = messageBody.replace(/\",\"\"/g, "")
-        messageBody = messageBody.replace(/,,/, ",")
-        messageBody = messageBody.replace(/\"\{/g, "{")
-        console.log("messageBody:::" + messageBody)
-    }
-
-    callback(messageBody)
 }
 /**
  * Generate manager action section(Your action)
