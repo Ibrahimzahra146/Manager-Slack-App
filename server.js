75c47993b07617b838fbf32b5ec3c2b99028c807
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-06-11 14:31:30 +0300

    sick flow

--- server.js
+++ server.js
@@ -897,22 +897,26 @@ function managerApproval1(msg, value, approvalType, fromManager, comment, reject
                 env.mRequests.getSlackRecord(userEmail, function (error, response, body) {
                   var responseBody = JSON.parse(body);
                   var slack_message = env.stringFile.slack_message(responseBody.userChannelId, responseBody.slackUserId, responseBody.teamId)
-                  if (type == "sick" && approvalType == "Approved" && sickReportFlag == 1)
+                  if (type == "sick" && approvalType == "Approved" && sickReportFlag == 1) {
                     feedback_message_to_emp = env.stringFile.upload_sick_report_message(userEmail, vacationId, fromDate, toDate, type)
-                  else {
-                    feedback_message_to_emp = messageSender.sendMessagetoEmpOnAction(msg, managerEmail, fromDate, toDate, userEmail, type, bot, approvalType, body, typeText, responseBody, comment);
+
+
+                    env.bot.startConversation(slack_message, function (err, convo) {
+
+                      if (!err) {
+                        var stringfy = JSON.stringify(feedback_message_to_emp);
+                        var obj1 = JSON.parse(stringfy);
+                        env.bot.reply(slack_message, obj1);
+
+                      }
+                    });
+                  } else {
+                    messageSender.sendMessagetoEmpOnAction(msg, managerEmail, fromDate, toDate, userEmail, type, bot, approvalType, body, typeText, responseBody, comment);
 
                   }
 
-                  env.bot.startConversation(slack_message, function (err, convo) {
 
-                    if (!err) {
-                      var stringfy = JSON.stringify(feedback_message_to_emp);
-                      var obj1 = JSON.parse(stringfy);
-                      env.bot.reply(slack_message, obj1);
 
-                    }
-                  });
                   replaceMessage.replaceMessage(msg, userEmail, managerEmail, fromDate, toDate, type, approvalType, vacationId, approvalId, ImageUrl, typeText, workingDays, managerApprovalsSection, JSON.parse(vacationBody).vacationState, JSON.parse(vacationBody).comments)
                   /* if (comment != "accept_with_report")
                      messageSender.sendMessagetoEmpOnAction(msg, managerEmail, fromDate, toDate, userEmail, type, bot, approvalType, body, typeText, responseBody, comment);
