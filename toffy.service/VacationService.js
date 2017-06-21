7a869b5c8764d284e850084ccebe43da86b9dfae
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-06-21 11:31:05 +0300

    accepet with report

--- toffy.service/VacationService.js
+++ toffy.service/VacationService.js
@@ -297,22 +297,17 @@ module.exports.vacationWithLeave = function vacationWithLeave(msg, response, ema
                         var validPreviousDate = 1;
 
                         if (timeMilliseconds.getFullYear() == 2018) {
-                            // toDate.setFullYear(2017)
 
-                            var res = dateHelper.getDayNumber(timeMilliseconds)
-                            if (res < 7 && res >= 0) {
-                                timeMilliseconds.setFullYear(2017)
-                            } else validPreviousDate = 0
+                            timeMilliseconds.setFullYear(2017)
+
                         }
                         timeMilliseconds = timeMilliseconds.getTime();
                         timeMilliseconds = timeMilliseconds - (3 * 60 * 60 * 1000);
                         toDate = new Date(toDate);
                         if (toDate.getFullYear() == 2018) {
-                            // toDate.setFullYear(2017)
-                            var res = dateHelper.getDayNumber(toDate)
-                            if (res < 7 && res >= 0) {
-                                toDate.setFullYear(2017)
-                            } else validPreviousDate = 0
+
+                            toDate.setFullYear(2017)
+
                         }
                         var dateMilliSeconds = toDate.getTime();
                         dateMilliSeconds = dateMilliSeconds - (3 * 60 * 60 * 1000)
