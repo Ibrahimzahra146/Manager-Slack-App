b2855fdc7c64b5fe18e809850222ea3bf0575067
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-06-21 13:46:37 +0300

    accepet with report

--- toffy.service/TextService.js
+++ toffy.service/TextService.js
@@ -1,20 +1,22 @@
 module.exports.prepareTextForApiAi = function prepareTextForApiAi(text, callback) {
-    console.log("text" + text)
-    var arr = text.toString().split('<')
-    var first_part = arr[0]
-    console.log("first_part" + first_part)
-    arr = text.toString().split('>')
-    var last_part = arr[1]
-    console.log("last_part" + last_part)
-    var mailPart = arr[0]
-    console.log("mailPart" + mailPart)
+    if ((text).indexOf('mailto') > -1) {
+        console.log("text" + text)
+        var arr = text.toString().split('<')
+        var first_part = arr[0]
+        console.log("first_part" + first_part)
+        arr = text.toString().split('>')
+        var last_part = arr[1]
+        console.log("last_part" + last_part)
+        var mailPart = arr[0]
+        console.log("mailPart" + mailPart)
 
-    mailPart = mailPart.toString().split('|')
-    mailPart = mailPart[1];
-    mailPart = mailPart.replace(/@exalt.ps/g, "");
-    mailPart = mailPart.replace(/>/g, "");
-    console.log("mailPart" + mailPart)
-    text = first_part + "" + mailPart + "" + last_part
+        mailPart = mailPart.toString().split('|')
+        mailPart = mailPart[1];
+        mailPart = mailPart.replace(/@exalt.ps/g, "");
+        mailPart = mailPart.replace(/>/g, "");
+        console.log("mailPart" + mailPart)
+        text = first_part + "" + mailPart + "" + last_part
 
-    callback(text)
+        callback(text)
+    } else callback(text)
 }
