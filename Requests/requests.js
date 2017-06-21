fe10eab5ae75b88bb12b3dd164a95463a0732128
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-06-21 16:36:17 +0300

    accepet with report

--- Requests/requests.js
+++ Requests/requests.js
@@ -209,10 +209,12 @@ module.exports.getUserSlackInfoBySlackId = function getUserSlackInfoBySlackId(id
         arr = id.toString().split('@')
         id = arr[1]
     }
+    console.log("getUserSlackInfoBySlackId" + id)
     if ((id).indexOf('>') > -1) {
         arr = id.toString().split('>')
         id = arr[0]
     }
+    console.log("getUserSlackInfoBySlackId" + id)
     var url = "https://slack.com/api/users.info?token=" + env.SLACK_ACCESS_TOKEN + "&user=" + id
     console.log("URL" + url)
     env.request({
