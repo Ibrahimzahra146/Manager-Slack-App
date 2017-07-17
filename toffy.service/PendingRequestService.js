4fd655e23f9ef995b2c3247985720e4501c51886
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-07-17 10:40:47 +0300

    show manager pending requests

--- toffy.service/PendingRequestService.js
+++ toffy.service/PendingRequestService.js
@@ -11,46 +11,57 @@ module.exports.showManagerPendingRequest = function showManagerPendingRequest(ms
                 }
                 else {
                     //build message Json result to send it to slack
-                    while ((JSON.parse(body)[i])) {
-                        console.log(JSON.stringify(body))
-                        console.log("body[i].id" + (JSON.parse(body))[i].id)
-                        console.log("body[i].email" + (JSON.parse(body))[i].employee.email)
-                        var userEmail = JSON.parse(body)[i].employee.email;
-                        var vacationId = JSON.parse(body)[i].id;
-                        var comment = JSON.parse(body)[i].comments;
-                        //var approvalId = arr[2]
-                        var managerEmail = email
-                        // var fromWho = ;
-                        var fromDate = JSON.parse(body)[i].fromDate;
-                        var toDate = JSON.parse(body)[i].toDate;
-                        var type = JSON.parse(body)[i].type
-                        var workingDays = JSON.parse(body)[i].period
-                        var ImageUrl = JSON.parse(body)[i].employee.profilePicture
-                        console.log("fromDate" + fromDate)
-                        console.log("toDate" + toDate)
+
+                    async.whilst(
+                        function () { return (JSON.parse(body)[i]) },
+                        function (callback) {
+                            console.log(JSON.stringify(body))
+                            console.log("body[i].id" + (JSON.parse(body))[i].id)
+                            console.log("body[i].email" + (JSON.parse(body))[i].employee.email)
+                            var userEmail = JSON.parse(body)[i].employee.email;
+                            var vacationId = JSON.parse(body)[i].id;
+                            var comment = JSON.parse(body)[i].comments;
+                            //var approvalId = arr[2]
+                            var managerEmail = email
+                            // var fromWho = ;
+                            var fromDate = JSON.parse(body)[i].fromDate;
+                            var toDate = JSON.parse(body)[i].toDate;
+                            var type = JSON.parse(body)[i].type
+                            var workingDays = JSON.parse(body)[i].period
+                            var ImageUrl = JSON.parse(body)[i].employee.profilePicture
+                            console.log("fromDate" + fromDate)
+                            console.log("toDate" + toDate)
 
 
-                        env.dateHelper.converDateToWords(fromDate, toDate, 0, function (fromDateWord, toDateWord) {
-                            console.log("fromDateWord1" + fromDateWord)
+                            env.dateHelper.converDateToWords(fromDate, toDate, 0, function (fromDateWord, toDateWord) {
+                                console.log("fromDateWord1" + fromDateWord)
 
-                            console.log("toDateWord1" + toDateWord)
+                                console.log("toDateWord1" + toDateWord)
 
-                            env.messageGenerator.generateManagerApprovelsSection(JSON.parse(body)[i].managerApproval, managerEmail, JSON.parse(body)[i].needsSickReport, function (managerApprovalsSection) {
-                                env.VacationHelper.getSecondApproverStateAndFinalState(managerEmail, JSON.parse(body)[i], 1, 1, function (myEmail, myAction, vacationState, approvalId) {
-                                    console.log("approvalId" + approvalId)
-                                    var messageBody = env.stringFile.sendVacationToManagerFunction(comment, ImageUrl, userEmail, fromDateWord, workingDays, toDateWord, type, approver2State, vacationId, approvalId, managerEmail, managerApprovalsSection, myAction, vacationState);
-                                    msg.say(messageBody)
+                                env.messageGenerator.generateManagerApprovelsSection(JSON.parse(body)[i].managerApproval, managerEmail, JSON.parse(body)[i].needsSickReport, function (managerApprovalsSection) {
+                                    env.VacationHelper.getSecondApproverStateAndFinalState(managerEmail, JSON.parse(body)[i], 1, 1, function (myEmail, myAction, vacationState, approvalId) {
+                                        console.log("approvalId" + approvalId)
+                                        var messageBody = env.stringFile.sendVacationToManagerFunction(comment, ImageUrl, userEmail, fromDateWord, workingDays, toDateWord, type, approver2State, vacationId, approvalId, managerEmail, managerApprovalsSection, myAction, vacationState);
+                                        msg.say(messageBody)
+                                    })
                                 })
+                                i++
+
+                                //  var message = env.stringFile.pendingVacationMessage(email, JSON.parse(body)[i].id, JSON.parse(body)[i].managerApproval, JSON.parse(body)[i].fromDate, JSON.parse(body)[i].toDate, JSON.parse(body)[i].type, fromDateWord, toDateWord)
+                                //  i++
+                                // msg.say(message)
+
                             })
-                            i++
 
-                            //  var message = env.stringFile.pendingVacationMessage(email, JSON.parse(body)[i].id, JSON.parse(body)[i].managerApproval, JSON.parse(body)[i].fromDate, JSON.parse(body)[i].toDate, JSON.parse(body)[i].type, fromDateWord, toDateWord)
-                            //  i++
-                            // msg.say(message)
+                            setTimeout(callback, 500);
 
-                        })
+                        },
+                        function (err) {
+                            // 5 seconds have passed
+                        }
 
-                    }
+                    );
+
 
                 }
 
