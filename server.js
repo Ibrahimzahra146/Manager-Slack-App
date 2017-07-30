8af4e3cb4a3273f1e4caf68dde5acd97c6842b1d
Ibrahim zahra <ibrahim.zahra@hotmail.com>
2017-07-30 09:17:41 +0300

    converted to personal

--- server.js
+++ server.js
@@ -886,7 +886,7 @@ app.post('/manager/pending-request-reminder', (req, res) => {
   console.log("Body:")
   console.log(req.body)
   console.log(JSON.stringify(req.body))
-  console.log(JSON.parse(req.body))
+  //console.log(JSON.parse(req.body))
   /*var parsedBody = JSON.parse(req.body)
   var vacationId = parsedBody.id
 
